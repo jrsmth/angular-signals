3d84f7e56451d6b5d8928997893cf46c30cc0d6e
jrsmth-tier2 <jsmith@tier2consulting.com>
2024-02-01 23:37:33 +0000

    Playing around with colours and layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,18 @@ import {RouterOutlet} from '@angular/router';
 import {SignalComponent} from "./signal/signal.component";
 import {SubjectComponent} from "./subject/subject.component";
 import {HighlightDirective} from "./core/directive/highlight.directive";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SignalComponent, SubjectComponent, HighlightDirective],
+  imports: [CommonModule, RouterOutlet, SignalComponent, SubjectComponent, HighlightDirective, FaIconComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-signals';
+  protected readonly faGithub = faGithub;
 }
